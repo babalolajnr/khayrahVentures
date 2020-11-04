3985d75f06acfc9b806116c001f692eff9dd4d75
Abdulqudduus Babalola <babalolajnr@gmail.com>
2020-11-04 13:58:25 +0100

    initial commit

---
+++ database/migrations/2020_11_04_122802_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->string('code');
+            $table->float('retail_price');
+            $table->float('wholesale_price');
+            $table->string('color')->nullable();
+            $table->foreignId('size_id')->nullable();
+            $table->foreignId('category_id')->nullable();
+            $table->foreignId('brand_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
