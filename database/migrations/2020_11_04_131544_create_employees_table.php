cea37e22f0155b0fb4a01a22aea1ac13d22d8b60
Abdulqudduus Babalola <babalolajnr@gmail.com>
2020-11-04 14:17:13 +0100

    created employees migration

---
+++ database/migrations/2020_11_04_131544_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone_number')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('slug')->nullable();
+            $table->foreignId('address_id')->nullable();
+            $table->foreignId('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
