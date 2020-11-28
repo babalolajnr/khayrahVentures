8179cb05d998f5eb5a9c2dc7e4b35a085a49624c
babalolajnr <babalolajnr@gmail.com>
2020-11-28 17:14:16 +0100

    created store method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,9 @@ use App\Models\Brand;
 use App\Models\ProductCategory;
 use App\Models\Size;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Validator;
 
 class ProductController extends Controller
 {
@@ -22,5 +24,54 @@ class ProductController extends Controller
             'sizes' => $sizes,
             'brands' => $brands
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $messages = [
+            'unique' => 'Size Exists',
+            'code.required_if' => 'Code field required when product category is mattresses',
+            'color.required_if' => 'Color field required when product category is mattresses'
+        ];
+
+        $this->validate($request, ([
+            'name'      => ['required'],
+            'code'      => ['required_if:productCategory,==, Mattresses'],
+            'productCategory' => ['required'],
+            'color'     => ['required_if:productCategory,==, Mattresses'],
+            'wholesale' =>  ['required'],
+            'retail'    =>  ['required'],
+            'size'      =>  ['required_if:productCategory,==, Mattresses'],
+            'brand'     =>  ['required']
+        ]), $messages);
+
+        $productCategory = $request->productCategory;
+        $productCategoryID = ProductCategory::where('name', $productCategory)->first();
+        $productCategoryID = $productCategoryID->id;
+        
+        $size = $request->size;
+        $sizeID = Size::where('name', $size)->first();
+        $sizeID = $sizeID->id;
+        
+        $brand = $request->brand;
+        $brandID = Brand::where('name', $brand)->first();
+        $brandID = $brandID->id;
+
+        // dd($productCategoryID->id);
+
+        Auth::user()->products()->create([
+            'name'              => $request->name,
+            'slug'              => 'lllll',
+            'code'              => $request->code,
+            'color'             => $request->color,
+            'wholesale_price'         => $request->wholesale,
+            'retail_price'            => $request->retail,
+            'size_id'           => $sizeID,
+            'brand_id'          => $brandID,
+            'product_category_id'   => $productCategoryID
+        ]);
+
+        return redirect('/addNewProduct');
+        
     }
 }
