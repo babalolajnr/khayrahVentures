9dc1cc4443935fa98ef86fa849c7d1c6887d173f
babalolajnr <babalolajnr@gmail.com>
2020-12-02 08:46:04 +0100

    added tests

---
+++ tests/Feature/ProductCategoriesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductCategoriesTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+
+    public function testStoreMethod()
+    {
+
+        $user = User::find(1);
+
+        $response = $this->actingAs($user)->post('/submitNewCategory', [
+            'name' => 'Pillows'
+        ]);
+
+        $response->assertStatus(302);
+    }
+
+    public function testFailedValidationMethod()
+    {
+        $user = User::find(1);
+        $response = $this->actingAs($user)->post('/submitNewCategory', []);
+
+        $response->assertSessionHasErrors('name');
+    }
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testUserLogin()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/dashboard');
+
+        $response->assertStatus(200);
+        // $response->assertInertia();
+    }
+}
