29313bab32d6a17efaa4e28a6485d2cdadb519a7
babalolajnr <babalolajnr@gmail.com>
2020-12-02 13:38:50 +0100

    removed unused class imports

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,11 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Brand;
 use App\Models\Product;
-use App\Models\ProductCategory;
-use App\Models\Size;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
