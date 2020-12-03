e46a169c598488e10140c418ae8964f7524bf48c
babalolajnr <babalolajnr@gmail.com>
2020-12-03 21:15:50 +0100

    added inventory test class

---
+++ tests/Feature/InventoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Arr;
+
+class InventoryTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testInventoryStoreMethod()
+    {
+        $user = User::factory()->create();
+        $product = Product::pluck('name')->all();
+        // $product = Product::factory()->create();
+        // dd($product);
+        $product = Arr::random($product);
+        // dd($product);
+        $quantity = mt_rand(0,100);
+
+        $response = $this->actingAs($user)->post('/submitInventory',[
+            'product'   => $product,
+            'quantity'  => $quantity
+        ]);
+
+        $response->assertStatus(302);
+    }
+}
