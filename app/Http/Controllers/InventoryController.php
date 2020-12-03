74b3e766c54c2da988f607ba7f6d226a1d155c9a
babalolajnr <babalolajnr@gmail.com>
2020-12-03 21:09:22 +0100

    created inventory controller

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class InventoryController extends Controller
 {
-    //
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, ([
+            'product'   =>  ['required'],
+            'quantity'  =>  ['required', 'integer']
+        ]));
+
+        $product = $request->product;
+        $product = Product::where('name', $product)->first();
+        $productID = $product->id;
+        $quantity = $request->quantity;
+
+        Auth::user()->inventories()->create([
+            'products_id'   =>  $productID,
+            'quantity'      =>  $quantity
+        ]);
+
+        return redirect('/inventory');
+    }
 }
