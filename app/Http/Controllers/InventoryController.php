f325b76bd562c14351fbdb192d536d329e92cda8
babalolajnr <babalolajnr@gmail.com>
2020-12-04 10:47:38 +0100

    minor changes

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class InventoryController extends Controller
@@ -24,13 +26,24 @@ class InventoryController extends Controller
         $product = $request->product;
         $product = Product::where('name', $product)->first();
         $productID = $product->id;
-        $quantity = $request->quantity;
 
-        Auth::user()->inventories()->create([
-            'products_id'   =>  $productID,
-            'quantity'      =>  $quantity
-        ]);
+        $checkInventory = Inventory::where('id', $productID)->first();
+        // dd($checkInventory);
 
-        return redirect('/inventory');
+        if (!empty($checkInventory)) {
+            throw $errors = ValidationException::withMessages([
+                'product' => 'Product Exists'
+            ]);
+        } else {
+
+            $quantity = $request->quantity;
+
+            Auth::user()->inventories()->create([
+                'products_id'   =>  $productID,
+                'quantity'      =>  $quantity
+            ]);
+
+            return redirect('/inventory');
+        }
     }
 }
