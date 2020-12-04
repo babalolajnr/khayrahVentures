89546aa99673d9496657c4e62745820e833572f9
babalolajnr <babalolajnr@gmail.com>
2020-12-04 10:48:12 +0100

    added inventory factory

--- database/factories/InventoryFactory.php
+++ database/factories/InventoryFactory.php
@@ -3,7 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\Inventory;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 class InventoryFactory extends Factory
 {
@@ -21,8 +24,15 @@ class InventoryFactory extends Factory
      */
     public function definition()
     {
+        $quantity = mt_rand(0,100);
+        $product = Product::pluck('id')->all();
+        $productID = $this->faker->unique()->randomElement($product);
+        $user = User::pluck('id')->all();
+        $userID = Arr::random($user);
         return [
-            //
+            'quantity' => $quantity,
+            'products_id' => $productID,
+            'user_id' => $userID
         ];
     }
 }
