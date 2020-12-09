eb3068108e0ad5150489be05d0a824fb0970f8d2
babalolajnr <babalolajnr@gmail.com>
2020-12-09 12:55:15 +0100

    added custom run functionality

--- database/seeders/InventorySeeder.php
+++ database/seeders/InventorySeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Inventory;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class InventorySeeder extends Seeder
 {
@@ -14,6 +17,25 @@ class InventorySeeder extends Seeder
      */
     public function run()
     {
-        Inventory::factory()->times(4)->create();
+        $product = Product::pluck('id')->all();
+        $productArrayLength = count($product);
+
+        //get a user that is an admin
+        $user = User::whereHas('userType', function ($q) {
+            $q->where('name', 'Admin');
+        })->pluck('id')->all();
+
+        
+        for ($i = 0; $i < $productArrayLength; $i++) {
+            $quantity = mt_rand(0, 100);
+            $userID = Arr::random($user);
+
+            Inventory::UpdateorCreate([
+                'products_id' => $product[$i]
+            ], [
+                'quantity'  => $quantity,
+                'user_id'   => $userID
+            ]);
+        }
     }
 }
