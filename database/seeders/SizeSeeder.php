8acf99b0cb15fe522334c061dfd07fcfecef8958
babalolajnr <babalolajnr@gmail.com>
2020-12-09 12:55:38 +0100

    added custom run functionality

--- database/seeders/SizeSeeder.php
+++ database/seeders/SizeSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Size;
+use App\Models\User;
 use Database\Factories\SizeFactory;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,33 @@ class SizeSeeder extends Seeder
      */
     public function run()
     {
-        Size::factory()->times(10)->create();
+        $sizes = [
+            '75inx54inx6in',
+            '75inx48inx6in',
+            '75inx42inx6in',
+            '75inx36inx6in',
+            '75inx54inx8in',
+            '75inx54inx8in',
+            '75inx48inx8in',
+            '75inx42inx8in',
+            '75inx36inx8in',
+            '75inx36inx4in',
+            '75inx30inx4in',
+            '75inx30inx3in',
+        ];
+        $sizesArrayLength = count($sizes);
+        $userID = User::factory()->create()->id;
+        for ($i = 0; $i < $sizesArrayLength; $i++) {
+            $size = $sizes[$i];
+            Size::updateorCreate(
+                [
+                    'name' => $size,
+                ],
+                [
+                    'user_id' => $userID
+
+                ]
+            );
+        }
     }
 }
