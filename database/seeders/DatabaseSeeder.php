5c9b8a66e3a0574cdfa7c9bc211502bd54ca5a68
babalolajnr <babalolajnr@gmail.com>
2020-12-09 12:54:27 +0100

    added usertypeseeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserTypeSeeder::class,
             UserSeeder::class,
             BrandSeeder::class,
             SizeSeeder::class,
