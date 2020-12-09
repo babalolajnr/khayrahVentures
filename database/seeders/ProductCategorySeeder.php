79a6f4a35b198093c5fcc6606d1dbfea8e1abd0d
babalolajnr <babalolajnr@gmail.com>
2020-12-09 12:55:27 +0100

    added custom run functionality

--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\ProductCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ProductCategorySeeder extends Seeder
@@ -14,6 +15,15 @@ class ProductCategorySeeder extends Seeder
      */
     public function run()
     {
-        ProductCategory::factory()->times(4)->create();
+        $categories = ['Mattresses', 'Pillows', 'Bedsheets', 'Duvets'];
+        $categoriesArrayLength = count($categories);
+        for ($i=0; $i < $categoriesArrayLength; $i++) { 
+            $category = $categories[$i];
+            $userID = User::factory()->create()->id;
+            ProductCategory::updateOrCreate([
+                'name' => $category,
+                'user_id' => $userID
+                ]);
+        }
     }
 }
