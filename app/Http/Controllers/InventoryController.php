cbfd7b9caf21539194efe665597128dede1a292c
babalolajnr <babalolajnr@gmail.com>
2020-12-10 19:39:29 +0100

    removed inventory store method

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -16,34 +16,34 @@ class InventoryController extends Controller
         //
     }
 
-    public function store(Request $request)
-    {
-        $this->validate($request, ([
-            'product'   =>  ['required'],
-            'quantity'  =>  ['required', 'integer']
-        ]));
+    // public function store(Request $request)
+    // {
+    //     $this->validate($request, ([
+    //         'product'   =>  ['required'],
+    //         'quantity'  =>  ['required', 'integer']
+    //     ]));
 
-        $product = $request->product;
-        $product = Product::where('name', $product)->first();
-        $productID = $product->id;
+    //     $product = $request->product;
+    //     $product = Product::where('name', $product)->first();
+    //     $productID = $product->id;
 
-        $checkInventory = Inventory::where('id', $productID)->first();
-        // dd($checkInventory);
+    //     $checkInventory = Inventory::where('id', $productID)->first();
+    //     // dd($checkInventory);
 
-        if (!empty($checkInventory)) {
-            throw $errors = ValidationException::withMessages([
-                'product' => 'Product Exists'
-            ]);
-        } else {
+    //     if (!empty($checkInventory)) {
+    //         throw $errors = ValidationException::withMessages([
+    //             'product' => 'Product Exists'
+    //         ]);
+    //     } else {
 
-            $quantity = $request->quantity;
+    //         $quantity = $request->quantity;
 
-            Auth::user()->inventories()->create([
-                'products_id'   =>  $productID,
-                'quantity'      =>  $quantity
-            ]);
+    //         Auth::user()->inventories()->create([
+    //             'products_id'   =>  $productID,
+    //             'quantity'      =>  $quantity
+    //         ]);
 
-            return redirect('/inventory');
-        }
-    }
+    //         return redirect('/inventory');
+    //     }
+    // }
 }
