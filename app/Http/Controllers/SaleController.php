a272a2d7cffad78f4c51ec4b7930e25bd0d9c4f8
babalolajnr <babalolajnr@gmail.com>
2020-12-12 21:48:59 +0100

    finalized store method

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Inventory;
+use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 
 class SaleController extends Controller
 {
     public function store(Request $request)
     {
-        $productAvailableQuantity = Inventory::where('products_id', $request->product_id)->pluck('quantity')->first();
+        $productAvailableQuantity = Inventory::where('product_id', $request->product_id)->pluck('quantity')->first();
 
         $this->validate($request, ([
             'product_id'                => ['required','integer'],
@@ -22,10 +25,47 @@ class SaleController extends Controller
             'address'                   => ['string']
         ]));
         
-        $productID = $request->product_id;
-        Auth::user()->sales()->create([
 
+        $slug = Str::of($request->firstname.' '.$request->lastname, '-');
+        //create customer
+        $customer = Auth::user()->customers()->create([
+            'firstname'     => $request->customer_firstname,
+            'lastname'      => $request->customer_lastname,
+            'phone_number'  => $request->customer_phone_number,
+            'email'         => $request->customer_email,
+            'slug'          => $slug,
+            'address'       => $request->address
         ]);
+
+
+
+        //create sale
+        $productID = $request->product_id;
+        $productPrice = Product::where('id', $productID)->pluck('retail_price')->first();
+        $quantity = $request->quantity;
+        $amount = $productPrice * $quantity;
+        // dd($customer->id);
+
+
+        $sale = Auth::user()->sales()->create([
+            'product_id'    => $productID,
+            'quantity_sold' => $quantity,
+            'amount'        => $amount,
+            'customer_id'   => $customer->id,
+        ]);
+        // $exception = new Exception;
+        // dd($exception->getMessage());
+
+        // dd($sale);
+
+
+        $quantitySold = $sale->quantity_sold;
+        $remainingQuantity = $productAvailableQuantity - $quantitySold;
+
+        Auth::user()->inventories()->where('product_id', $productID)->update([
+            'quantity' => $remainingQuantity        
+        ]);
+
         return response(200);
     }
 }
