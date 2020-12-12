f5d8e274ca4dbdd4afd04fea270938c6f8d32a68
babalolajnr <babalolajnr@gmail.com>
2020-12-12 12:36:51 +0100

    removed store method

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -20,37 +20,6 @@ class InventoryController extends Controller
         return response($inventory, 200);
     }
 
-    // public function store(Request $request)
-    // {
-    //     $this->validate($request, ([
-    //         'product'   =>  ['required'],
-    //         'quantity'  =>  ['required', 'integer']
-    //     ]));
-
-    //     $product = $request->product;
-    //     $product = Product::where('name', $product)->first();
-    //     $productID = $product->id;
-
-    //     $checkInventory = Inventory::where('id', $productID)->first();
-    //     // dd($checkInventory);
-
-    //     if (!empty($checkInventory)) {
-    //         throw $errors = ValidationException::withMessages([
-    //             'product' => 'Product Exists'
-    //         ]);
-    //     } else {
-
-    //         $quantity = $request->quantity;
-
-    //         Auth::user()->inventories()->create([
-    //             'products_id'   =>  $productID,
-    //             'quantity'      =>  $quantity
-    //         ]);
-
-    //         return redirect('/inventory');
-    //     }
-    // }
-
     public function edit ($id)
     {
         $product = Inventory::findorFail($id);
