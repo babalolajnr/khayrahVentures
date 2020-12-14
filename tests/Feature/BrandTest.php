ddb32fd062b4643ac8ad175163a80fde1216df6c
babalolajnr <babalolajnr@gmail.com>
2020-12-14 15:39:41 +0100

    implemented test generator class

--- tests/Feature/BrandTest.php
+++ tests/Feature/BrandTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Models\Brand;
 use App\Models\User;
 use App\Models\UserType;
+use App\Utilities\TestUserGenerator;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Arr;
@@ -13,31 +15,49 @@ use Tests\TestCase;
 class BrandTest extends TestCase
 {
     use WithFaker;
+    use DatabaseTransactions;
 
     public function testEditBrand()
     {
-        $userType = UserType::where('name', 'Admin')->first();
-        $userType = $userType->id;
-        $user = User::factory()->create(['user_type_id' => $userType]);
+        $user = TestUserGenerator::generateAdminUser();
         $brand = Brand::pluck('id')->all();
         $brandID = Arr::random($brand);
 
-        $request = $this->actingAs($user)->get('/editBrand/'.$brandID);
+        $request = $this->actingAs($user)->get('/editBrand/' . $brandID);
 
         $request->assertStatus(200);
-        
     }
 
     public function testAdminCanStoreBrand()
     {
-        $userType = UserType::where('name', 'Admin')->first();
-        $userType = $userType->id;
-        $user = User::factory()->create(['user_type_id' => $userType]);
-        
+        $user = TestUserGenerator::generateAdminUser();
         $request = $this->actingAs($user)->post('/submitNewBrand', [
-            'name'=> $this->faker->company
+            'name' => $this->faker->company
         ]);
-        
+
         $request->assertStatus(302);
+    }
+
+    public function testNonAdminCannotStoreBrand()
+    {
+
+        $user =  TestUserGenerator::generateNonAdminUser();
+        $request = $this->actingAs($user)->post('/submitNewBrand', [
+            'name' => $this->faker->company
+        ]);
+
+        $request->assertStatus(403);
+    }
+
+    public function testAdminCanUpdateBrand()
+    {
+        $user = TestUserGenerator::generateAdminUser();
+        $brand = Brand::pluck('id')->all();
+        $brandID = Arr::random($brand);
+        $request = $this->actingAs($user)->patch('/updateBrand/'. $brandID, [
+            'name' => $this->faker->company
+        ]);
+
+        $request->assertStatus(200);
     }
 }
