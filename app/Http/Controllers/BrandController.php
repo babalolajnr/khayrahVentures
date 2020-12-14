94030a1979d689f94d4d10c90bce7ae0b8e7c846
babalolajnr <babalolajnr@gmail.com>
2020-12-14 14:04:15 +0100

    added admin authorization

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -13,24 +14,30 @@ class BrandController extends Controller
     public function index()
     {
         return Inertia::render('AddNewBrand');
-
     }
 
     public function store(Request $request)
     {
+        $this->authorize('create', Product::class );
+
         $messages = [
             'unique' => 'Brand Exists'
-        ];  
+        ];
 
         $this->validate($request, array(
             'name' => 'required|unique:brands',
         ), $messages);
 
         Auth::user()->brands()->create([
-            'name' => $request->name, 
+            'name' => $request->name,
         ]);
 
         return redirect('/addNewBrand');
+    }
 
+    public function edit($id)
+    {
+        $brand = Brand::findOrFail($id);
+        return response(200);
     }
 }
