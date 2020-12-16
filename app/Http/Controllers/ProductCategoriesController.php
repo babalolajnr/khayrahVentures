d65ff30edf345b775701a838adaf9fc6f77581fd
babalolajnr <babalolajnr@gmail.com>
2020-12-16 18:13:35 +0100

    added update method

--- app/Http/Controllers/ProductCategoriesController.php
+++ app/Http/Controllers/ProductCategoriesController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ProductCategoriesController extends Controller
 {
-    
+
     /**
      * DONE
      * index
@@ -19,28 +20,51 @@ class ProductCategoriesController extends Controller
      * update
      * destroy
      */
-    public function index () 
+    public function index()
     {
         return Inertia::render('AddNewCategory');
     }
 
-    public function store(Request $request) 
+    public function store(Request $request)
     {
+        $this->authorize('create', ProductCategory::class);
 
-        $messages = [
-            'unique' => 'Category Exists'
-        ];
-
-        $this->validate($request, array(
-            'name' => 'required|unique:products_category'
-        ), $messages);
-
+        ProductCategory::validateIncomingRequest($request);
 
         Auth::user()->productCategories()->create([
             'name' => $request->name
         ]);
 
         return redirect('/addNewCategory');
+    }
+
+    public function edit($id)
+    {
+        $productCategory = ProductCategory::findorFail($id);
+        return response(200);
+    }
+
+    public function update($id, Request $request, ProductCategory $productCategory)
+    {
+        $this->authorize('update', $productCategory);
+
+        ProductCategory::validateIncomingRequest($request);
+
+        $productCategory = ProductCategory::findOrFail($id);
+
+        $productCategory->name = $request->name;
+
+        $productCategory->save();
+
+        return response(200);
+    }
+
+    public function destroy($id, ProductCategory $productCategory)
+    {
+        $this->authorize('delete', $productCategory);
+
+        $productCategory = ProductCategory::findOrFail($id);
+        $productCategory->delete();
         
     }
 }
