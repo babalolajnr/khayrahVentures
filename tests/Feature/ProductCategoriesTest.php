0faa71e96795e90b3ae746bb305218078a1aa0e5
babalolajnr <babalolajnr@gmail.com>
2020-12-16 18:19:00 +0100

    added delete product category test

--- tests/Feature/ProductCategoriesTest.php
+++ tests/Feature/ProductCategoriesTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature;
 
+use App\Models\ProductCategory;
 use App\Models\User;
 use App\Utilities\TestUserGenerator;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
 use Tests\TestCase;
 
 class ProductCategoriesTest extends TestCase
@@ -17,7 +19,8 @@ class ProductCategoriesTest extends TestCase
      * @return void
      */
     // use RefreshDatabase;
-    use DatabaseTransactions;
+    // use DatabaseTransactions;
+    use withFaker;
 
     //SEED DATABASE BEFORE RUNNING TESTS
     public function testStoreMethod()
@@ -26,19 +29,66 @@ class ProductCategoriesTest extends TestCase
         // $this->withoutExceptionHandling();
         $user = TestUserGenerator::generateAnyUser();
 
-        $response = $this->actingAs($user)->post('/submitNewCategory', [
+        $request = $this->actingAs($user)->post('/submitNewCategory', [
             'name' => 'Pillows'
         ]);
 
-        $response->assertStatus(302);
+        $request->assertStatus(302);
     }
 
     public function testFailedValidationMethod()
     {
         // $this->withoutExceptionHandling();
         $user = TestUserGenerator::generateAnyUser();
-        $response = $this->actingAs($user)->post('/submitNewCategory', []);
+        $request = $this->actingAs($user)->post('/submitNewCategory', []);
 
-        $response->assertSessionHasErrors('name');
+        $request->assertSessionHasErrors('name');
+    }
+
+    public function testEditProductCategory()
+    {
+        $user = TestUserGenerator::generateAdminUser();
+        $productCategory = ProductCategory::pluck('id')->all();
+        $productCategoryID = Arr::random($productCategory);
+
+        $request = $this->actingAs($user)->get('/editProductCategory/'. $productCategoryID);
+        $request->assertStatus(200);
+    }
+
+    public function testUpdateProductCategory()
+    {
+        $user = TestUserGenerator::generateAdminUser();
+        $productCategory = ProductCategory::pluck('id')->all();
+        $productCategoryID = Arr::random($productCategory);
+
+        $request = $this->actingAs($user)->patch('/updateProductCategory/'. $productCategoryID, [
+            'name' => $this->faker->word
+        ]);
+
+        $request->assertStatus(200);
+    }
+
+    public function testNonAdminCannotUpdateProductCategory()
+    {
+        $user = TestUserGenerator::generateNonAdminUser();
+        $productCategory = ProductCategory::pluck('id')->all();
+        $productCategoryID = Arr::random($productCategory);
+
+        $request = $this->actingAs($user)->patch('/updateProductCategory/'. $productCategoryID, [
+            'name' => $this->faker->word
+        ]);
+
+        $request->assertStatus(403);
+    }
+
+    public function testAdminCanDeleteProductCategory()
+    {
+        $user = TestUserGenerator::generateAdminUser();
+        $productCategory = ProductCategory::pluck('id')->all();
+        $productCategoryID = Arr::random($productCategory);
+
+        $request = $this->actingAs($user)->delete('/deleteProductCategory/'. $productCategoryID);
+
+        $request->assertStatus(200);
     }
 }
