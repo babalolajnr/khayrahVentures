be2b75ddcf6ceb640540f3ce12262db2ddd57480
babalolajnr <babalolajnr@gmail.com>
2020-12-21 10:13:48 +0100

    removed egisterinertia method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,28 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerInertia();
+        //
     }
 
-    //create shared data to be viewed from any page in the app
-    public function registerInertia()
-    {
-        Inertia::share([
-            'user' => function () {
-                $user = auth()->user();
-                return $user ? [
-                    'firstname' => $user->firstname,
-                    'lastname' => $user->lastname,
-                    'phoneNumber' => $user->phone_number,
-                    'email' => $user->email,
-                    'userType' => $user->userType->name,
-                    'can' => [
-                        'create_product' => $user->can('create', Product::class),
-                    ]
-                ] : null;
-            }
-        ]);
-    }
     /**
      * Bootstrap any application services.
      *
