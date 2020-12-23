c221b891c1578123989a496c9e12f2ba995a2089
babalolajnr <babalolajnr@gmail.com>
2020-12-23 09:06:17 +0100

    chnaged the seeding order and added some warning comments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        /**
+         * WARNING: The seeder classes have to 
+         * follow the exact order in which they are listed here for 
+         * there to be no issues.
+         * 1. UserTypeSeeder
+         * 2. UserSeeder
+         * 3. BrandSeeder
+         * 4. SizeSeeder
+         * 5. ProductCategorySeeder
+         * 6. InventorySeeder
+         * 7. ProductSeeder
+         */
+        
         $this->call([
             UserTypeSeeder::class,
             UserSeeder::class,
             BrandSeeder::class,
             SizeSeeder::class,
             ProductCategorySeeder::class,
+            InventorySeeder::class,
             ProductSeeder::class,
-            InventorySeeder::class
         ]);
     }
 }
