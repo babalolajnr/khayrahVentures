26b5f86b71ac4e8f7ef9b50e91a7d076af2973db
babalolajnr <babalolajnr@gmail.com>
2020-12-23 08:49:09 +0100

    added bootstrap-vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,16 @@ import Vue from 'vue';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 const app = document.getElementById('app');
 
