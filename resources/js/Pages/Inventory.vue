32aa3904d05fe6af97abcdfc779053e99676dba8
babalolajnr <babalolajnr@gmail.com>
2020-12-23 09:07:51 +0100

    added b-table

--- resources/js/Pages/Inventory.vue
+++ resources/js/Pages/Inventory.vue
@@ -28,9 +28,7 @@
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">
-                    DataTable with minimal features &amp; hover style
-                  </h3>
+                    Inventory
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body">
@@ -44,264 +42,7 @@
                     </div>
                     <div class="row">
                       <div class="col-sm-12">
-                        <table
-                          id="example2"
-                          class="table table-bordered table-hover dataTable dtr-inline"
-                          role="grid"
-                          aria-describedby="example2_info"
-                        >
-                          <thead>
-                            <tr role="row">
-                              <th
-                                class="sorting_asc"
-                                tabindex="0"
-                                aria-controls="example2"
-                                rowspan="1"
-                                colspan="1"
-                                aria-sort="ascending"
-                                aria-label="Rendering engine: activate to sort column descending"
-                              >
-                                Rendering engine
-                              </th>
-                              <th
-                                class="sorting"
-                                tabindex="0"
-                                aria-controls="example2"
-                                rowspan="1"
-                                colspan="1"
-                                aria-label="Browser: activate to sort column ascending"
-                              >
-                                Browser
-                              </th>
-                              <th
-                                class="sorting"
-                                tabindex="0"
-                                aria-controls="example2"
-                                rowspan="1"
-                                colspan="1"
-                                aria-label="Platform(s): activate to sort column ascending"
-                              >
-                                Platform(s)
-                              </th>
-                              <th
-                                class="sorting"
-                                tabindex="0"
-                                aria-controls="example2"
-                                rowspan="1"
-                                colspan="1"
-                                aria-label="Engine version: activate to sort column ascending"
-                              >
-                                Engine version
-                              </th>
-                              <th
-                                class="sorting"
-                                tabindex="0"
-                                aria-controls="example2"
-                                rowspan="1"
-                                colspan="1"
-                                aria-label="CSS grade: activate to sort column ascending"
-                              >
-                                CSS grade
-                              </th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr role="row" class="odd">
-                              <td class="dtr-control sorting_1" tabindex="0">
-                                Gecko
-                              </td>
-                              <td>Firefox 1.0</td>
-                              <td>Win 98+ / OSX.2+</td>
-                              <td>1.7</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="even">
-                              <td class="dtr-control sorting_1" tabindex="0">
-                                Gecko
-                              </td>
-                              <td>Firefox 1.5</td>
-                              <td>Win 98+ / OSX.2+</td>
-                              <td>1.8</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="odd">
-                              <td class="dtr-control sorting_1" tabindex="0">
-                                Gecko
-                              </td>
-                              <td>Firefox 2.0</td>
-                              <td>Win 98+ / OSX.2+</td>
-                              <td>1.8</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="even">
-                              <td class="dtr-control sorting_1" tabindex="0">
-                                Gecko
-                              </td>
-                              <td>Firefox 3.0</td>
-                              <td>Win 2k+ / OSX.3+</td>
-                              <td>1.9</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="odd">
-                              <td class="sorting_1 dtr-control">Gecko</td>
-                              <td>Camino 1.0</td>
-                              <td>OSX.2+</td>
-                              <td>1.8</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="even">
-                              <td class="sorting_1 dtr-control">Gecko</td>
-                              <td>Camino 1.5</td>
-                              <td>OSX.3+</td>
-                              <td>1.8</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="odd">
-                              <td class="sorting_1 dtr-control">Gecko</td>
-                              <td>Netscape 7.2</td>
-                              <td>Win 95+ / Mac OS 8.6-9.2</td>
-                              <td>1.7</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="even">
-                              <td class="sorting_1 dtr-control">Gecko</td>
-                              <td>Netscape Browser 8</td>
-                              <td>Win 98SE+</td>
-                              <td>1.7</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="odd">
-                              <td class="sorting_1 dtr-control">Gecko</td>
-                              <td>Netscape Navigator 9</td>
-                              <td>Win 98+ / OSX.2+</td>
-                              <td>1.8</td>
-                              <td>A</td>
-                            </tr>
-                            <tr role="row" class="even">
-                              <td class="sorting_1 dtr-control">Gecko</td>
-                              <td>Mozilla 1.0</td>
-                              <td>Win 95+ / OSX.1+</td>
-                              <td>1</td>
-                              <td>A</td>
-                            </tr>
-                          </tbody>
-                          <tfoot>
-                            <tr>
-                              <th rowspan="1" colspan="1">Rendering engine</th>
-                              <th rowspan="1" colspan="1">Browser</th>
-                              <th rowspan="1" colspan="1">Platform(s)</th>
-                              <th rowspan="1" colspan="1">Engine version</th>
-                              <th rowspan="1" colspan="1">CSS grade</th>
-                            </tr>
-                          </tfoot>
-                        </table>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-sm-12 col-md-5">
-                        <div
-                          class="dataTables_info"
-                          id="example2_info"
-                          role="status"
-                          aria-live="polite"
-                        >
-                          Showing 1 to 10 of 57 entries
-                        </div>
-                      </div>
-                      <div class="col-sm-12 col-md-7">
-                        <div
-                          class="dataTables_paginate paging_simple_numbers"
-                          id="example2_paginate"
-                        >
-                          <ul class="pagination">
-                            <li
-                              class="paginate_button page-item previous disabled"
-                              id="example2_previous"
-                            >
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="0"
-                                tabindex="0"
-                                class="page-link"
-                                >Previous</a
-                              >
-                            </li>
-                            <li class="paginate_button page-item active">
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="1"
-                                tabindex="0"
-                                class="page-link"
-                                >1</a
-                              >
-                            </li>
-                            <li class="paginate_button page-item">
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="2"
-                                tabindex="0"
-                                class="page-link"
-                                >2</a
-                              >
-                            </li>
-                            <li class="paginate_button page-item">
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="3"
-                                tabindex="0"
-                                class="page-link"
-                                >3</a
-                              >
-                            </li>
-                            <li class="paginate_button page-item">
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="4"
-                                tabindex="0"
-                                class="page-link"
-                                >4</a
-                              >
-                            </li>
-                            <li class="paginate_button page-item">
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="5"
-                                tabindex="0"
-                                class="page-link"
-                                >5</a
-                              >
-                            </li>
-                            <li class="paginate_button page-item">
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="6"
-                                tabindex="0"
-                                class="page-link"
-                                >6</a
-                              >
-                            </li>
-                            <li
-                              class="paginate_button page-item next"
-                              id="example2_next"
-                            >
-                              <a
-                                href="#"
-                                aria-controls="example2"
-                                data-dt-idx="7"
-                                tabindex="0"
-                                class="page-link"
-                                >Next</a
-                              >
-                            </li>
-                          </ul>
-                        </div>
+                        <b-table striped hover :items="items"></b-table>
                       </div>
                     </div>
                   </div>
@@ -326,10 +67,7 @@ import Layout from "@/Layouts/Layout.vue";
 import { FulfillingSquareSpinner } from "epic-spinners";
 export default {
   props: {
-    errors: Object,
-    productCategories: Array,
-    sizes: Array,
-    brands: Array,
+    inventory: Array
   },
   components: {
     Layout,
@@ -339,56 +77,42 @@ export default {
     return {
       date: new Date().getFullYear(),
       success: false,
-      form: {
-        name: null,
-        code: null,
-        color: null,
-        wholesale: null,
-        retail: null,
-        productCategory: null,
-        size: null,
-        brand: null,
-        description: null,
-      },
+      // items: this.$props.inventory,
+      fields: [
+        {
+          key: "index",
+          sortable: true
+        },
+        {
+          key: 'name',
+          sortable: true
+        },
+        {
+          key: 'code',
+          sortable: true
+        },
+        {
+          key: 'retail_price',
+          label: "Retail Price",
+          sortable: true
+        },
+        {
+          key: 'wholesale_price',
+          label: "Wholesale Price",
+          sortable: true
+        },
+        {
+          key: 'color',
+          label: "Color",
+          sortable: true
+        }
+
+      ],
       loading: false,
-      formError: false,
     };
   },
   methods: {
-    submit() {
-      this.loading = true;
-      if (this.form.name == null) {
-        this.formError = true;
-      } else {
-        this.formError = false;
-        this.$inertia.post("/submitNewProduct", this.form);
-        this.$inertia.on("success", (event) => {
-          //check if the errors props is empty
-          if (Object.entries(this.$props.errors).length > 0) {
-            // this.clearForm()
-          } else {
-            this.success = true;
-            this.clearForm();
-            setTimeout(this.successMessageFade, 2000);
-          }
-        });
-      }
-      this.loading = false;
-    },
-    successMessageFade() {
-      this.success = false;
-    },
-    clearForm() {
-      this.form.name = "";
-      this.form.code = "";
-      this.form.color = "";
-      this.form.wholesale = "";
-      this.form.retail = "";
-      this.form.productCategory = "";
-      this.form.size = "";
-      this.form.brand = "";
-      this.form.description = "";
-    },
+    
   },
   computed: {
     checkIfUserIsAuthorized() {
@@ -399,6 +123,3 @@ export default {
   },
 };
 </script>
-
-<style>
-</style>
