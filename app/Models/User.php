0eeccb56c6df536eb4c634c2fdb6f000cde6afad
babalolajnr <babalolajnr@gmail.com>
2020-12-30 16:17:39 +0100

    added mustVerifyEmail interface

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements mustVerifyEmail
 {
     use HasApiTokens;
     use HasFactory;
@@ -29,6 +29,7 @@ class User extends Authenticatable
         'phone_number',
         'email',
         'password',
+        'user_type_id'
     ];
 
     /**
